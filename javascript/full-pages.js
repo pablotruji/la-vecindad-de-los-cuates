3667cc678cfe2caae1afddbf25cc845d37e2cb13
Pablo <ptrujillo@udlanet.ec>
2020-03-02 07:51:04 -0500

    Hemos implementado el slider en la pagina cuates mas chidos uanque debido al full page tenemos problemas en la vision en crome en movil

--- javascript/full-pages.js
+++ javascript/full-pages.js
@@ -1,4 +1,4 @@
-const seccionesPagina = new  fullpage('#fullpage',{
+const seccionesPagina = new fullpage('#fullpage',{
     licenseKey: '519C3159-45DB46FE-ABF27203-B0A573F4',
 
     //Navegación
---
+++ javascript/slide-cuates-mas-chidos.js
@@ -0,0 +1,15 @@
+const seccionesPagina = new fullpage('#fullpage',{
+    licenseKey: '519C3159-45DB46FE-ABF27203-B0A573F4',
+
+    menu: '#menu',
+    lockAnchors: false,
+    anchors:['firstPage', 'SecondPage'],
+    navigation: false,
+	// navigationPosition: 'right',
+	// navigationTooltips: ['inicio', '1', '2', '3', '4'],
+	showActiveTooltip: true,
+	slidesNavigation: false,
+    slidesNavPosition: 'bottom',
+	responsiveWidth: 200,
+    loopBottom: false
+});
