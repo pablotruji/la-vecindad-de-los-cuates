a646b71b4be9378ad70482b02914defda81294a9
Pablo <ptrujillo@udlanet.ec>
2020-04-16 10:52:32 -0500

    Implementadp barra menu de escritorio

--- javascript/eventos-instagram.js
+++ javascript/eventos-instagram.js
@@ -11,6 +11,10 @@ function nFormatter(num){
   return num;
   
 }
+
+var x = window.matchMedia("(max-width: 1022px)");
+var y = window.matchMedia("(min-width: 1023px)");
+
 $.ajax({
   url:"https://www.instagram.com/lavecindad/?__a=1",
   type:'get',
@@ -28,10 +32,32 @@ $.ajax({
       url = posts[i].node.display_url;
       likes = posts[i].node.edge_liked_by.count;
       comments = posts[i].node.edge_media_to_comment.count;
-      posts_html += '<div class="col-sm-4 equal-height"><img style="display:block; margin:auto; min-height:40px;background-color:#fff;width:100%" src="'+url+'"><div class="row like-comment"><div class="col-md-4">'+nFormatter(likes)+' LIKES</div><div class="col-md-6">'+nFormatter(comments)+' COMMENTS</div></div></div>';
-    }
-    $(".posts").html(posts_html);
-    console.log(this.url);
-  }
-  
+      if (x.matches){
+        posts_html += '<div class="col-sm-6 equal-height"><img style="display:block; margin:auto; min-height:40px;background-color:#fff;width:100%" src="'+url+'"><div class="row like-comment"><div class="col-md-4">'+nFormatter(likes)+' LIKES</div><div class="col-md-6">'+nFormatter(comments)+' COMMENTS</div></div></div>';
+        $(".posts").html(posts_html);
+      }
+    } 
+      for(var i=0;i<posts.length;i++){
+        url = posts[i].node.display_url;
+        likes = posts[i].node.edge_liked_by.count;
+        comments = posts[i].node.edge_media_to_comment.count;
+        if(y.matches){
+          posts_html += '<div class="col-sm-3 equal-height"><img style="display:block; margin:auto; min-height:40px;background-color:#fff;width:100%" src="'+url+'"><div class="row like-comment"><div class="col-md-4">'+nFormatter(likes)+' LIKES</div><div class="col-md-6">'+nFormatter(comments)+' COMMENTS</div></div></div>';
+          $(".posts").html(posts_html);
+        }
+      } 
+    } 
 });
+
+
+
+// function myFunction(x) {
+//   if (x.matches) { // If media query matches
+//       document.body.style.backgroundColor = "yellow";
+//   } else {
+//       document.body.style.backgroundColor = "pink";
+//   }   
+// }
+
+// myFunction(x)
+// x.addListener(myFunction)
